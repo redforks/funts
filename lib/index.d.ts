a490127655f1dec65885b22a9447a7dfd6c241bd
Red Forks <redforks@gmail.com>
2018-10-02 15:34:58 +0800

    Add StringKeys generic type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,3 +2,7 @@ export * from './actFuncs';
 export declare type DeepPartial<T> = {
     [K in keyof T]?: T[K] extends Array<infer R> ? Array<DeepPartial<R>> : DeepPartial<T[K]>;
 };
+/**
+ * Alias of Extract<keyof T, string>.
+ */
+export declare type StringKeys<T> = Extract<keyof T, string>;
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export * from './actFuncs';
 export type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends Array<infer R> ? Array<DeepPartial<R>> : DeepPartial<T[K]>
 };
+
+/**
+ * Alias of Extract<keyof T, string>.
+ */
+export type StringKeys<T> = Extract<keyof T, string>;
